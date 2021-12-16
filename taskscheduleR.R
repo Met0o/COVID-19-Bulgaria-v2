615c54bd343d89b79086305f1143a65b81189c7b
Met0o <metodi.simeonov@gmail.com>
2021-12-16 11:56:21 +0200

    Minor updates

--- taskscheduleR.R
+++ taskscheduleR.R
@@ -1,7 +1,7 @@
 library(taskscheduleR)
                      
 taskscheduler_create(taskname = "COVID-19-Automation", rscript = "C://R/COVID/Updates.R", 
-                     schedule = "DAILY", starttime = "12:00", startdate = format(Sys.Date(), "%m/%d/%Y"))
+                     schedule = "DAILY", starttime = "11:00", startdate = format(Sys.Date(), "%m/%d/%Y"))
 
 # taskscheduler_delete(taskname = "COVID")
  
